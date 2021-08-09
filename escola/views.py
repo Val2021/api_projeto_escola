d65ae48a6ee4f3fd36c3baa1e74e7091fef3bf3a
Val Araujo <val.araujo@businessplanner.com.br>
2021-08-09 09:52:34 -0300

    Implementada classes viewset

--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,14 @@
 from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
 
+class AlunosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os alunos e alunas """
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Guilherme'}
-        return JsonResponse(aluno)
+class CursosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
