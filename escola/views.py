aae321c442c41b8eab40b78550b6b94191a05396
Val Araujo <val.araujo@businessplanner.com.br>
2021-08-15 10:07:08 -0300

    Adicionado versionamento de api

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,6 +10,11 @@ class AlunosViewSet(viewsets.ModelViewSet):
     serializer_class = AlunoSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    def get_serializer_class(self):
+        if self.request.version =='v2':
+            return AlunoSerializerV2
+        else:
+            return AlunoSerializer
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
