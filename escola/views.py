847b5dcac17dd484f9eaf8ff63cd6c18d00f646b
Val Araujo <val.araujo@businessplanner.com.br>
2021-08-16 09:05:19 -0300

    Inserindo Location no Headers

--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets, generics
+from rest_framework import status, viewsets, generics
+from rest_framework import response
+from rest_framework.serializers import Serializer
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
-
+from rest_framework.response import Response
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -18,6 +20,14 @@ class CursosViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
     
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     """Listando todas as matrículas"""
@@ -42,4 +52,4 @@ class ListaAlunosMatriculados(generics.ListAPIView):
         return queryset
     serializer_class = ListaAlunosMatriculadosSerializer
    
-   
+
